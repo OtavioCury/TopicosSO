4da04a61e902fd0fa02bcdfa9ec67a3524f9e057
OtavioCury <otaviocury.oc@gmail.com>
2016-11-21 12:04:14 -0300

    Feito o algoritmo ótimo

--- src/algoritmos/OTM.java
+++ src/algoritmos/OTM.java
@@ -1,5 +1,114 @@
 package algoritmos;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import modelo.Pagina;
+
 public class OTM {
+	private Queue<Pagina> filaPaginasOriginal;
+	private Queue<Pagina> filaPaginas;
+	private int quantMolduras;
+	private int faltaPaginas;
+
+	public OTM(Queue<Pagina> filaPaginasOriginal, int quantMolduras) {
+		super();
+		this.filaPaginasOriginal = filaPaginasOriginal;
+		this.quantMolduras = quantMolduras;
+		faltaPaginas = 0;
+	}
+
+
+	public void executa() {
+		// TODO Auto-generated method stub
+		filaPaginas = new LinkedList<Pagina>();
+
+		for (int i = 0; i < quantMolduras; i++) {
+			faltaPaginas++;
+			filaPaginas.offer(filaPaginasOriginal.peek());
+			filaPaginasOriginal.poll();
+		}
+
+		while (!filaPaginasOriginal.isEmpty()) {
+			boolean presente = false;
+
+			for (Pagina pagina : filaPaginas) {
+				if (pagina.getId() == filaPaginasOriginal.peek().getId()) {
+					presente = true;
+				}
+			}
+
+			if (presente == false) {
+				faltaPaginas++;
+				for (Pagina pagina : filaPaginas) {
+					contaReferencias(pagina);
+				}
+				filaPaginas.remove(maiorReferencia(filaPaginas));
+				filaPaginas.offer(filaPaginasOriginal.peek());
+				filaPaginasOriginal.poll();
+			}else{
+				filaPaginasOriginal.poll();
+			}
+		}
+		System.out.println(faltaPaginas);
+	}
+
+	public Pagina maiorReferencia(Queue<Pagina> filaPaginas){
+
+		Pagina maior = filaPaginas.peek();		
+
+		for (Pagina pagina : filaPaginas) {
+			if (pagina.getProximaReferencia() > maior.getProximaReferencia()) {
+				maior = pagina;
+			}
+		}
+
+		return maior;
+	}
+
+	public void contaReferencias(Pagina pagina){
+
+		int quant = 0;
+		forReferencia: for (Pagina paginaOriginal : filaPaginasOriginal) {
+			quant++;
+			if (paginaOriginal.getId() == pagina.getId()) {
+				pagina.setProximaReferencia(quant);
+				break forReferencia;
+			}
+		}
+
+	}
+
+	public Queue<Pagina> getFilaPaginas() {
+		return filaPaginas;
+	}
+
+	public void setFilaPaginas(Queue<Pagina> filaPaginas) {
+		this.filaPaginas = filaPaginas;
+	}
+
+	public int getQuantMolduras() {
+		return quantMolduras;
+	}
+
+	public void setQuantMolduras(int quantMolduras) {
+		this.quantMolduras = quantMolduras;
+	}
+
+	public Queue<Pagina> getFilaPaginasOriginal() {
+		return filaPaginasOriginal;
+	}
+
+	public void setFilaPaginasOriginal(Queue<Pagina> filaPaginasOriginal) {
+		this.filaPaginasOriginal = filaPaginasOriginal;
+	}
+
+	public int getFaltaPaginas() {
+		return faltaPaginas;
+	}
+
+	public void setFaltaPaginas(int faltaPaginas) {
+		this.faltaPaginas = faltaPaginas;
+	}		
 
 }
--- src/main/Executa.java
+++ src/main/Executa.java
@@ -4,31 +4,49 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 import algoritmos.FIFO;
+import algoritmos.OTM;
 import modelo.Pagina;
 
 public class Executa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub		
-		Queue<Pagina> filaPagina = new LinkedList<Pagina>();
+		Queue<Pagina> filaPaginaFifo = new LinkedList<Pagina>();
+		Queue<Pagina> filaPaginaOtm = new LinkedList<Pagina>();
 		int quantMolduras = 4;
 
-		filaPagina.offer(new Pagina(1, 0));
-		filaPagina.offer(new Pagina(2, 0));
-		filaPagina.offer(new Pagina(3, 0));
-		filaPagina.offer(new Pagina(4, 0));
-		filaPagina.offer(new Pagina(1, 0));
-		filaPagina.offer(new Pagina(2, 0));
-		filaPagina.offer(new Pagina(5, 0));
-		filaPagina.offer(new Pagina(1, 0));
-		filaPagina.offer(new Pagina(2, 0));
-		filaPagina.offer(new Pagina(3, 0));
-		filaPagina.offer(new Pagina(4, 0));
-		filaPagina.offer(new Pagina(5, 0));
+		filaPaginaFifo.offer(new Pagina(1, 0));
+		filaPaginaFifo.offer(new Pagina(2, 0));
+		filaPaginaFifo.offer(new Pagina(3, 0));
+		filaPaginaFifo.offer(new Pagina(4, 0));
+		filaPaginaFifo.offer(new Pagina(1, 0));
+		filaPaginaFifo.offer(new Pagina(2, 0));
+		filaPaginaFifo.offer(new Pagina(5, 0));
+		filaPaginaFifo.offer(new Pagina(1, 0));
+		filaPaginaFifo.offer(new Pagina(2, 0));
+		filaPaginaFifo.offer(new Pagina(3, 0));
+		filaPaginaFifo.offer(new Pagina(4, 0));
+		filaPaginaFifo.offer(new Pagina(5, 0));
 
-		FIFO fifo = new FIFO(filaPagina, quantMolduras);
+		FIFO fifo = new FIFO(filaPaginaFifo, quantMolduras);
 
 		fifo.executa();
+
+		filaPaginaOtm.offer(new Pagina(1, 0));
+		filaPaginaOtm.offer(new Pagina(2, 0));
+		filaPaginaOtm.offer(new Pagina(3, 0));
+		filaPaginaOtm.offer(new Pagina(4, 0));
+		filaPaginaOtm.offer(new Pagina(1, 0));
+		filaPaginaOtm.offer(new Pagina(2, 0));
+		filaPaginaOtm.offer(new Pagina(5, 0));
+		filaPaginaOtm.offer(new Pagina(1, 0));
+		filaPaginaOtm.offer(new Pagina(2, 0));
+		filaPaginaOtm.offer(new Pagina(3, 0));
+		filaPaginaOtm.offer(new Pagina(4, 0));
+		filaPaginaOtm.offer(new Pagina(5, 0));
+
+		OTM otm = new OTM(filaPaginaOtm, quantMolduras);
+		otm.executa();
 
 	}
 
