6e3bcdfd7d233655a59f144b103b9d5f323d631a
OtavioCury <otaviocury.oc@gmail.com>
2016-12-07 02:50:12 +0000

    Lendo da entrada padrão

--- src/main/Executa.java
+++ src/main/Executa.java
@@ -1,41 +1,50 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import algoritmos.Elevador;
 import algoritmos.FCFS;
 import algoritmos.SSF;
+import algoritmos.Elevador;
 
 public class Executa {
 	public static void main(String[] args) {
 		List<Integer> cilindros = new ArrayList<Integer>();
 		List<Integer> cilindros2 = new ArrayList<Integer>();
-		int quantCilindros = 199, primeiroCilindro = 53;
+		int quantCilindros = 0, primeiroCilindro = 0, i = 0;
 
-		cilindros.add(98);
-		cilindros.add(183);
-		cilindros.add(37);
-		cilindros.add(122);
-		cilindros.add(14);
-		cilindros.add(124);
-		cilindros.add(65);
-		cilindros.add(67);
+		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+		String x = null;
+
+		try {
+			while( (x = f.readLine()) != null )
+			{				
+				Integer numero = Integer.parseInt(x.trim());
+				if (i == 0) {
+					quantCilindros = numero;
+					i++;
+				}else if (i == 1) {
+					primeiroCilindro = numero;
+					i++;
+				}else{
+					cilindros.add(numero);
+					cilindros2.add(numero);
+					i++;
+				}	
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		FCFS fcfs = new FCFS(cilindros, quantCilindros, primeiroCilindro);
 		fcfs.executa();
 
 		SSF ssf = new SSF(cilindros, quantCilindros, primeiroCilindro);
 		ssf.executa();
-
-		cilindros2.add(98);
-		cilindros2.add(183);
-		cilindros2.add(37);
-		cilindros2.add(122);
-		cilindros2.add(14);
-		cilindros2.add(124);
-		cilindros2.add(65);
-		cilindros2.add(67);
 
 		Elevador elevador = new Elevador(cilindros2, quantCilindros, primeiroCilindro);
 		elevador.executa();
