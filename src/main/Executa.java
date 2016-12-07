274486327b448cbb5d416bb10daf4fe49a5b9449
OtavioCury <otaviocury.oc@gmail.com>
2016-12-07 02:54:28 +0000

    Lendo da entrada padrão

--- src/main/Executa.java
+++ src/main/Executa.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -16,52 +19,35 @@ public class Executa {
 		Queue<Pagina> filaPaginaOtm = new LinkedList<Pagina>();
 		Queue<Pagina> filaPaginaLRU = new LinkedList<Pagina>();
 
-		int quantMolduras = 4;
+		int quantMolduras = 0, i = 0;
 
-		filaPaginaFifo.offer(new Pagina(1, 0));
-		filaPaginaFifo.offer(new Pagina(2, 0));
-		filaPaginaFifo.offer(new Pagina(3, 0));
-		filaPaginaFifo.offer(new Pagina(4, 0));
-		filaPaginaFifo.offer(new Pagina(1, 0));
-		filaPaginaFifo.offer(new Pagina(2, 0));
-		filaPaginaFifo.offer(new Pagina(5, 0));
-		filaPaginaFifo.offer(new Pagina(1, 0));
-		filaPaginaFifo.offer(new Pagina(2, 0));
-		filaPaginaFifo.offer(new Pagina(3, 0));
-		filaPaginaFifo.offer(new Pagina(4, 0));
-		filaPaginaFifo.offer(new Pagina(5, 0));
+		BufferedReader f = new BufferedReader(new InputStreamReader(System.in));
+		String x = null;
+
+		try {
+			while( (x = f.readLine()) != null )
+			{				
+				Integer numero = Integer.parseInt(x.trim());
+				if (i == 0) {
+					quantMolduras = numero;
+					i++;
+				}else{
+					filaPaginaFifo.offer(new Pagina(numero, 0));
+					filaPaginaLRU.offer(new Pagina(numero, 0));
+					filaPaginaOtm.offer(new Pagina(numero, 0));
+					i++;
+				}	
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}				
 
 		FIFO fifo = new FIFO(filaPaginaFifo, quantMolduras);
 		fifo.executa();
 
-		filaPaginaOtm.offer(new Pagina(1, 0));
-		filaPaginaOtm.offer(new Pagina(2, 0));
-		filaPaginaOtm.offer(new Pagina(3, 0));
-		filaPaginaOtm.offer(new Pagina(4, 0));
-		filaPaginaOtm.offer(new Pagina(1, 0));
-		filaPaginaOtm.offer(new Pagina(2, 0));
-		filaPaginaOtm.offer(new Pagina(5, 0));
-		filaPaginaOtm.offer(new Pagina(1, 0));
-		filaPaginaOtm.offer(new Pagina(2, 0));
-		filaPaginaOtm.offer(new Pagina(3, 0));
-		filaPaginaOtm.offer(new Pagina(4, 0));
-		filaPaginaOtm.offer(new Pagina(5, 0));
-
 		OTM otm = new OTM(filaPaginaOtm, quantMolduras);
 		otm.executa();
-
-		filaPaginaLRU.offer(new Pagina(1, 0));
-		filaPaginaLRU.offer(new Pagina(2, 0));
-		filaPaginaLRU.offer(new Pagina(3, 0));
-		filaPaginaLRU.offer(new Pagina(4, 0));
-		filaPaginaLRU.offer(new Pagina(1, 0));
-		filaPaginaLRU.offer(new Pagina(2, 0));
-		filaPaginaLRU.offer(new Pagina(5, 0));
-		filaPaginaLRU.offer(new Pagina(1, 0));
-		filaPaginaLRU.offer(new Pagina(2, 0));
-		filaPaginaLRU.offer(new Pagina(3, 0));
-		filaPaginaLRU.offer(new Pagina(4, 0));
-		filaPaginaLRU.offer(new Pagina(5, 0));
 
 		LRU lru = new LRU(filaPaginaLRU, quantMolduras); 
 		lru.executa();
